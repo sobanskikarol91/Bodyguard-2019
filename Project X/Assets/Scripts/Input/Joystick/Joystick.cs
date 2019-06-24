555a65ffafaa72f5487a2ba3f40a5ecd4d29a85a
Karol Sobański <sobanskikarol91@gmail.com>
2019-06-24 23:46:04 +0200

    Add: InputManager

--- Project X/Assets/Scripts/Input/Joystick/Joystick.cs
+++ Project X/Assets/Scripts/Input/Joystick/Joystick.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Joystick : MonoBehaviour, IDirectionInput
@@ -8,61 +9,53 @@ public abstract class Joystick : MonoBehaviour, IDirectionInput
     [SerializeField] Transform circle;
     [SerializeField] Transform innerCircle;
 
+    public event Action OnUse = delegate { };
+    public Vector2 Direction { get; private set; }
+
     private Vector2 pressPos, currentFingerPos;
     private new Camera camera;
     private Transform player;
 
-    public event Action OnUse = delegate { };
-    public Vector2 Direction { get; private set; }
+
+
+    private Touch touch;
+    public int FingerId { get; private set; }
 
 
     private void Start()
     {
-        SetInputDependsOnPlatform();
         player = GameManager.instance.Player.transform;
         camera = Camera.main;
     }
 
-    private void SetInputDependsOnPlatform()
-    {
-
-    }
-
-    private void OnMouseButtonPressing()
-    {
-        UpdateJoystickPosition();
-        currentFingerPos = GetWorldMousePos();
-        OnUse();
-    }
-
     private Vector3 GetWorldMousePos()
     {
-        return camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.transform.position.z));
+        return camera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, camera.transform.position.z));
     }
 
-    private void Update()
-    {
-#if UNITY_EDITOR
-        if (Input.GetMouseButtonDown(0) && IsTouchConditionMet())
-            OnMouseButtonDown();
-        else if (Input.GetMouseButton(0) && IsTouchConditionMet())
-            OnMouseButtonPressing();
-        else if (Input.GetMouseButtonUp(0) && IsTouchConditionMet())
-            OnMouseButtonUp();
-#endif
-#if UNITY_ANDROID
 
-    }
 
     private void OnMouseButtonDown()
     {
+        Debug.Log("Down");
+        FingerId = touch.fingerId;
         pressPos = GetWorldMousePos();
         circle.transform.position = pressPos;
         circle.gameObject.SetActive(true);
     }
 
+    private void OnMouseButtonPressing()
+    {
+        Debug.Log("isTouching");
+        UpdateJoystickPosition();
+        currentFingerPos = GetWorldMousePos();
+        OnUse();
+    }
+
     private void OnMouseButtonUp()
     {
+        Debug.Log("Up");
+        FingerId = -1;
         circle.gameObject.SetActive(false);
     }
 
