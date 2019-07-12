9be06be41d76d44015f667dc98307357dfb0bbd3
Karol Sobański <sobanskikarol91@gmail.com>
2019-07-12 16:42:30 +0200

    Add; Shrinking Ability

---
+++ Project X/Assets/Scripts/Enemy/ShrinkingEnemy/ShrinkingEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class ShrinkingEnemy : MonoBehaviour
+{
+    [SerializeField] float timeToChangeState;
+
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    //private IEnumerable ChangeState()
+    //{
+    //                private float leftTimeToChangeState;
+
+    //yield return null;
+    //}
+}
