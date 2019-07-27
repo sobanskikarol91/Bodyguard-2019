e0db993376f7691933d3cece183a4492952a5996
Karol Sobański <sobanskikarol91@gmail.com>
2019-07-27 12:46:16 +0200

    [Enemy Weapon] Add: Red Triple bullets

--- Project X/Assets/Scripts/Weapons/Weapon/SimpleWeapon.cs
+++ Project X/Assets/Scripts/Weapons/Weapon/SimpleWeapon.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
+﻿using UnityEngine;
+
 
 [CreateAssetMenu(fileName = "Weapon", menuName = "Weapon/Simple")]
 public class SimpleWeapon : Weapon
