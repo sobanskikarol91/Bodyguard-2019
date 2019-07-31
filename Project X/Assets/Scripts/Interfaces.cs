10b5076a1ab33457e7229f4f211e3179e6883931
Karol Sobański <sobanskikarol91@gmail.com>
2019-07-31 13:03:38 +0200

    [ScoreManager] Remove: Redundant reference

--- Project X/Assets/Scripts/Interfaces.cs
+++ Project X/Assets/Scripts/Interfaces.cs
@@ -7,3 +7,8 @@ public interface ITwoAxisInput
 {
     TwoAxisInput Input { get; set; }
 }
+
+public interface IScore
+{
+   void UpdateScore(float score);
+}
--- Project X/Assets/Scripts/Managers/ScoreManager.cs
+++ Project X/Assets/Scripts/Managers/ScoreManager.cs
@@ -6,20 +6,13 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] TextMeshPro scoreTxt;
 
     private int score = 0;
-    private ExpierienceManager expierienceManager;
     public delegate void UpdateScore(float score);
     public event UpdateScore ScoreAdded;
 
 
-    private void Awake()
-    {
-        expierienceManager = GetComponent<ExpierienceManager>();
-    }
-
     public void AddScore(int amount = 1)
     {
         score += amount;
-        expierienceManager?.AddExpierience(amount);
         UpdateUIText();
         ScoreAdded(score);
     }
