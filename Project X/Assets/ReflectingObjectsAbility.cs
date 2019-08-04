2ce6bbb7393f83fb9a4ba8a3c6da64706e07cb90
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-04 14:28:42 +0200

    [Enemy] Add: EnemyReflectingBullets

---
+++ Project X/Assets/ReflectingObjectsAbility.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class ReflectingObjectsAbility : MonoBehaviour
+{
+    [SerializeField] ObjectType[] Reflectingtypes;
+
+    private Collision2D collision;
+    private readonly Vector3 reverseDirection = new Vector3(0, 0, 180);
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.collision = collision;
+        InteractiveObject interactive = collision.gameObject.GetComponent<InteractiveObject>();
+
+        if (IsInteractive() && Reflectingtypes.Contains(interactive.Type))
+            Reflect();
+    }
+
+    private void Reflect()
+    {
+        collision.transform.Rotate(reverseDirection);
+    }
+
+    private bool IsInteractive()
+    {
+        return collision.gameObject.GetComponent<InteractiveObject>();
+    }
+}
