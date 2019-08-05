56dbd5b852864eb0b73e290d7483f18d4d355f38
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-05 10:50:35 +0200

    [MovingType] Add: MoveToPlayer

---
+++ Project X/Assets/Scripts/Abilities/Moving/MovingTypes/MoveToPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MoveToPlayer", menuName = "Moving/MoveToPlayer")]
+public class MoveToPlayer : MoveType
+{
+    private Player player;
+
+    public override void Init(MovingAbility ability)
+    {
+        base.Init(ability);
+        player = GameManager.instance.Player;
+    }
+
+    public override void Execute()
+    {
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        transform.position += (Vector3)(ability.Speed * direction * Time.deltaTime);
+    }
+}
