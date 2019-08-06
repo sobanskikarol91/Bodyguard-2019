1e330de9a70e41bce85aa03311170cbcef4941a4
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-06 14:39:46 +0200

    [RepellingForce] Add: Repelling force on collision

---
+++ Project X/Assets/Scripts/Repelling/RepellingOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RepellingOnCollision : MonoBehaviour
+{
+    [SerializeField] float force = 0.1f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")  return;
+
+        //Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+        Vector3 direction = (Vector3)collision.contacts[0].point - transform.position;
+        //rb.AddForce(direction * force, ForceMode2D.Impulse);
+        collision.transform.position += direction * force;
+    }
+}
