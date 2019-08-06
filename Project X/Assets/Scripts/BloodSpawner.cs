dc5f11195c8a74e172b568faf595eb25fb8fc72a
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-06 23:01:34 +0200

    [BloodSplatter] Add: Particle & sprites

--- Project X/Assets/Scripts/BloodSpawner.cs
+++ Project X/Assets/Scripts/BloodSpawner.cs
@@ -1,20 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "BloodSpawner", menuName = "Effects/BloodSpawner")]
 public class BloodSpawner : DeathEffect
 {
     [SerializeField] GameObject[] blood;
+    [SerializeField] GameObject particle;
 
     public override void CreateEffect(Transform transform)
     {
         GameObject prefab = ChooseRandom();
         GameObject instance = ObjectPoolManager.instance.Get(prefab);
         instance.transform.position = transform.position;
+        SetRandomRotation(transform);
+        SetFlip(transform);
+
+        GameObject particleInstance = ObjectPoolManager.instance.Get(particle);
+        particleInstance.transform.position = transform.position;
+    }
+
+    private void SetFlip(Transform transform)
+    {
+        
+    }
+
+    void SetRandomRotation(Transform transform)
+    {
+
     }
 
     GameObject ChooseRandom()
     {
-        int nr = Random.Range(0, blood.Length);
+        int nr = UnityEngine.Random.Range(0, blood.Length);
         return blood[nr];
     }
 }
