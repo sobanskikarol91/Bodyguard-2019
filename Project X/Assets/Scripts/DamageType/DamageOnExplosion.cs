ec2e1a11a77efaa6420a13969c71017d10024875
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-07 18:05:05 +0200

    [Weapon] Rocket Luncher ->Disable/Enable Collider

--- Project X/Assets/Scripts/DamageType/DamageOnExplosion.cs
+++ Project X/Assets/Scripts/DamageType/DamageOnExplosion.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Linq;
 
-public class DamageOnExplosion : Damagable 
+public class DamageOnExplosion : Damagable
 {
+    private new Collider2D collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         InteractiveObject interactive = collision.gameObject.GetComponent<InteractiveObject>();
@@ -10,5 +17,12 @@ public class DamageOnExplosion : Damagable
         if (!interactive) return;
         else if (!damageObjects.Contains(interactive.Type)) return;
         interactive.GetComponent<HealthAbility>()?.DoDamage(Damage);
+
+        collider.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        collider.enabled = true;
     }
 }
