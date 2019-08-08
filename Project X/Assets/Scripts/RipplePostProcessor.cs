508e3141863db48a07be16807b2d11826d8c172d
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-08 23:49:39 +0200

    [Effect] Add: Ripple base effect

---
+++ Project X/Assets/Scripts/RipplePostProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class RipplePostProcessor : MonoBehaviour
+{
+    public Material RippleMaterial;
+    public float MaxAmount = 50f;
+
+    [Range(0, 1)]
+    public float Friction = .9f;
+
+    private float Amount = 0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Amount = MaxAmount;
+            Vector3 pos = Input.mousePosition;
+            RippleMaterial.SetFloat("_CenterX", pos.x);
+            RippleMaterial.SetFloat("_CenterY", pos.y);
+        }
+
+        RippleMaterial.SetFloat("_Amount", Amount);
+        Amount *= Friction;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, RippleMaterial);
+    }
+}
