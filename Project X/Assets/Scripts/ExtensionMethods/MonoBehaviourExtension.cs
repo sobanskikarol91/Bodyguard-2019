c1b5e30f8374da42d8e55a786b65461005ade037
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-10 13:59:27 +0200

    [ExtensionMethods] Add: MonobehaviourExtension -> Lerp

---
+++ Project X/Assets/Scripts/ExtensionMethods/MonoBehaviourExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public static class MonoBevahiourExtension
+{
+    public static void StartLerp(this MonoBehaviour target, float origin, float destination, float duration, Action OnComplete = null)
+    {
+        target.StartCoroutine(IELerp(origin, destination, duration, OnComplete));
+    }
+
+    static IEnumerator IELerp(float origin, float destination, float duration, Action OnComplete = null)
+    {
+        float startTime = Time.time;
+
+        while (true)
+        {
+            float percantage = (Time.time - startTime) / duration;
+            origin = Mathf.Lerp(origin, destination, percantage);
+
+            if (percantage >= 1)
+                break;
+
+            yield return null;
+        }
+
+        OnComplete?.Invoke();
+    }
+}
