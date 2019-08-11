dc511ef8c9a15a3d53290a0022b600280d07954d
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-11 20:14:28 +0200

    [Enemy] Add: Eyes follow the player

---
+++ Project X/Assets/Scripts/EyesFollower/EyesFallower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class EyesFallower : MonoBehaviour
+{
+    [SerializeField] float distance = 1f;
+    [SerializeField] bool isAI;
+
+    Func<Vector2> GetDirection;
+
+    private void Awake()
+    {
+        if (isAI)
+            GetDirection = () => GameManager.instance.Player.transform.position - transform.position;
+        else
+            GetDirection = () => GameManager.instance.Platform.GetPlayerInputDependsOnPlatform().Rotating.Direction;
+    }
+
+    private void Update()
+    {
+        Vector2 direction = GetDirection().normalized;
+        transform.localPosition = direction * distance;
+    }
+}
---
+++ Project X/Assets/Scripts/RotateConstrains.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotateConstrains : MonoBehaviour 
+{
+    private Quaternion originRotation;
+
+    private void Awake()
+    {
+        originRotation = transform.rotation;    
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation = originRotation;
+    }
+}
