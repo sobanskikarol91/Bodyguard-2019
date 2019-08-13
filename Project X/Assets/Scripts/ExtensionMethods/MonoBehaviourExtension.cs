c5bd4613b1f2725d61406eb9d3d02fa37d32afbf
Karol Sobański <sobanskikarol91@gmail.com>
2019-08-13 21:37:11 +0200

    [LerpExtenshion] Add: LerpPingPong

--- Project X/Assets/Scripts/ExtensionMethods/MonoBehaviourExtension.cs
+++ Project X/Assets/Scripts/ExtensionMethods/MonoBehaviourExtension.cs
@@ -28,4 +28,47 @@ public static class MonoBevahiourExtension
 
         OnComplete?.Invoke();
     }
+
+    public static void StartLerp(this MonoBehaviour target, Vector3 origin, Vector3 destination, float duration, Action OnComplete = null, Action<Vector3> OnExecuting = null)
+    {
+        target.StartCoroutine(Lerp(origin, destination, duration, OnComplete, OnExecuting));
+    }
+
+    static IEnumerator Lerp(Vector3 currentValue, Vector3 destination, float duration, Action OnComplete = null, Action<Vector3> OnExecuting = null)
+    {
+        float startTime = Time.time;
+        Vector3 startValue = currentValue;
+
+        Debug.Log("Origin: " + currentValue + "Dest: " + destination + "Dur: " + duration);
+        while (true)
+        {
+            float percantage = (Time.time - startTime) / duration;
+            currentValue = Vector3.Lerp(startValue, destination, percantage);
+
+            OnExecuting?.Invoke(currentValue);
+            if (percantage >= 1)
+                break;
+
+            yield return null;
+        }
+
+        OnComplete?.Invoke();
+    }
+
+    public static void PingPongLerp(this MonoBehaviour target, Vector3 origin, Vector3 offset, float duration, Action OnComplete = null, Action<Vector3> OnExecuting = null)
+    {
+        target.StartCoroutine(IEPingPongLerp(target, origin, offset, duration, OnComplete, OnExecuting));
+    }
+
+    static IEnumerator IEPingPongLerp(this MonoBehaviour target, Vector3 currentValue, Vector3 offset, float duration, Action OnComplete = null, Action<Vector3> OnExecuting = null)
+    {
+        Debug.Log("ping-pong");
+        Vector3 destination = currentValue - offset;
+        float time = duration / 2;
+        Func<IEnumerator> Ping = () => Lerp(currentValue, destination, time, OnComplete, OnExecuting);
+        Func<IEnumerator> Pong = () => Lerp(destination, currentValue, time, OnComplete, OnExecuting);
+
+        yield return target.StartCoroutine(Ping());
+        yield return target.StartCoroutine(Pong());
+    }
 }
