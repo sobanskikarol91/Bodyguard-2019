3325b9965bb1d83fb04677ed8a7af7b587c57caa
Karol Sobański <sobanskikarol91@gmail.com>
2019-09-01 19:14:31 +0200

    [Enemy] Add: Big mom and child

---
+++ Project X/Assets/Scripts/DeathEffects/CreateObjectsAfterDeath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HealthAbility))]
+public class CreateObjectsAfterDeath : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] int amount;
+
+    private void Awake()
+    {
+        GetComponent<HealthAbility>().Death += SpawnObjects;
+    }
+
+    void SpawnObjects()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject instance =  ObjectPoolManager.instance.Get(prefab);
+            instance.transform.position = transform.position;
+        }
+    }
+}
